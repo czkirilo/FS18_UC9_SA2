30afb7db8be08b905f6b5a0a3f9f354d50a2593f
czkirilo <caiquez.kirilo@hotmail.com>
2022-12-12 20:48:49 -0300

    ER4

--- EPlayers/js/script.js
+++ EPlayers/js/script.js
@@ -1,3 +1,50 @@
+
+$(document).ready(function(){
+
+    let slideAtual = 1
+    let listaSlides = ["banner-f1", "banner-cs", "banner-lol"]
+
+    setInterval(mudarSlide, 5000)
+
+    function mudarSlide(){
+        //remover slide anterior
+        if(slideAtual > 0){
+            $("#carrossel").removeClass(listaSlides[slideAtual - 1])
+        }else{
+            $("#carrossel").removeClass(listaSlides[listaSlides.length - 1])
+        }      
+        
+
+        //exibir slide atual
+        $("#carrossel").addClass(listaSlides[slideAtual])
+
+        //indicar qual slide atual
+        console.log("Slide atual é", slideAtual)
+        slideAtual ++
+
+        if(slideAtual > listaSlides.length - 1){
+            slideAtual = 0
+        }
+
+    }
+
+
+
+
+
+
+
+
+    $("#barras").click(function(){
+        if($("#menu").hasClass("menu-ativo")){
+            $("#menu").removeClass("menu-ativo")
+        }else{
+            $("#menu").addClass("menu-ativo")
+        }
+    })
+})
+
+
 //alert("Arquivo externo utilizando js nesta pagina")
 
 function cadastrarNewsletter(){
@@ -5,16 +52,16 @@ function cadastrarNewsletter(){
     alert(email)
 }
 
-function mostrarMenu($event){
-    //identificar o elemento menu
-    let menu = document.getElementById("menu")
+// function mostrarMenu($event){
+//     //identificar o elemento menu
+//     let menu = document.getElementById("menu")
 
-    if(getComputedStyle(menu).display == "none"){
-        menu.style.display = "flex"
-    }
-    else{
-        menu.style.display = "none"
-    }
+//     if(getComputedStyle(menu).display == "none"){
+//         menu.style.display = "flex"
+//     }
+//     else{
+//         menu.style.display = "none"
+//     }
 
-    event.preventDefault()
-}
+//     event.preventDefault()
+// }
